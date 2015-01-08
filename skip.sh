616633854a85133306182ebc9b7a12f3a921c048
aershov <codumentary.com@gmail.com>
2015-01-08 22:46:13 +0300

    Use local vars
    To avoid polluting the global var namespace.

--- skip.sh
+++ skip.sh
@@ -3,8 +3,8 @@ skip () {
     echo "Usage: skip n-lines-to-skip [file ...]"
     return 1
   fi
-  n_lines_to_skip="$1"
-  start_with_line=$((n_lines_to_skip+1))
+  local n_lines_to_skip="$1"
+  local start_with_line=$((n_lines_to_skip+1))
   shift
 
   tail "+$start_with_line" "$@"
