51bd56812943f48cb20666d1ac7cc8ed0c99d3fc
aershov <codumentary.com@gmail.com>
2015-01-10 23:17:40 +0300

    Add TODOs

--- skip.sh
+++ skip.sh
@@ -1,3 +1,4 @@
+# TODO: add .travis.yml
 skip () {
   local usage="Usage: skip n-lines-to-skip [file ...]"
   if [ "$#" = 0 ]; then
@@ -8,6 +9,7 @@ skip () {
     -h|--help) echo "$usage"; return;;
   esac
 
+  # TODO: check that $1 is a number
   local n_lines_to_skip="$1"
   local start_with_line=$((n_lines_to_skip+1))
   shift
