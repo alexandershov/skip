476730fd047a8a2ef2cc08dd122bd901e343327a
aershov <codumentary.com@gmail.com>
2015-01-10 23:17:17 +0300

    Show usage when given -h|--help option

--- skip.sh
+++ skip.sh
@@ -1,8 +1,13 @@
 skip () {
+  local usage="Usage: skip n-lines-to-skip [file ...]"
   if [ "$#" = 0 ]; then
-    echo "Usage: skip n-lines-to-skip [file ...]"
+    echo "$usage"
     return 1
   fi
+  case "$1" in
+    -h|--help) echo "$usage"; return;;
+  esac
+
   local n_lines_to_skip="$1"
   local start_with_line=$((n_lines_to_skip+1))
   shift
